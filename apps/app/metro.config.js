5f4cc69fa68899dec7df5eb1881e27c9d4115a9b
홍서현 <ghdtjgus76@naver.com>
2024-12-18 00:53:54 +0900

    Fix/#29 React Native 실행 안 되는 문제 해결 (#30)
    * feat: 의존성 설치
    
    * feat: @react-native/gradle-plugin 설치
    
    * fix: pnpm이 react-native 프로젝트에서 node_modules 찾지 못하는 문제 해결

--- apps/app/metro.config.js
+++ apps/app/metro.config.js
@@ -1,11 +1,18 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    unstable_enableSymlinks: true,
+    unstable_enablePackageExports: true,
+  },
+  watchFolders: [path.join(__dirname, '..', '..')],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
