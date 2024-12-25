2a078a0e9d5fdc370e4a8d9c25ee85d301c8110e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-12-25 21:57:35 +0900

    refactor(packages/utils): 에러 핸들링 부분 HTTPError 객체로 리팩토링

--- packages/utils/src/fetcher/fetcher.ts
+++ packages/utils/src/fetcher/fetcher.ts
@@ -1,4 +1,5 @@
 import { isEqual } from "lodash";
+import HTTPError from "../errorHandler/HTTPError.ts";
 
 type ApiResponse<T> = Response & { data?: T; success?: boolean };
 
@@ -227,48 +228,7 @@ class Fetcher {
 
   private handleError(response: Response): Error | undefined {
     if (!response.ok) {
-      const error = new Error();
-
-      switch (response.status) {
-        case 400:
-          error.name = "Bad Request";
-          error.message = "The request was invalid or cannot be served.";
-          break;
-        case 401:
-          error.name = "Unauthorized";
-          error.message =
-            "Authentication is required and has failed or has not yet been provided.";
-          break;
-        case 403:
-          error.name = "Forbidden";
-          error.message =
-            "The request was valid, but the server is refusing action.";
-          break;
-        case 404:
-          error.name = "Not Found";
-          error.message = "The requested resource could not be found.";
-          break;
-        case 500:
-          error.name = "Internal Server Error";
-          error.message = "An error occurred on the server.";
-          break;
-        case 502:
-          error.name = "Bad Gateway";
-          error.message =
-            "The server received an invalid response from the upstream server.";
-          break;
-        case 503:
-          error.name = "Service Unavailable";
-          error.message =
-            "The server is currently unable to handle the request.";
-          break;
-        default:
-          error.name = "HTTP Error";
-          error.message = `An unexpected HTTP error occurred: ${response.status}`;
-          break;
-      }
-
-      return error;
+      return HTTPError.handle(response);
     }
   }
 
