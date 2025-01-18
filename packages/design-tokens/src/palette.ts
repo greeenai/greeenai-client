3ebbf975bb811b3154c6db18d777970e48805418
ghdtjgus76 <ghdtjgus76@naver.com>
2025-01-18 19:22:42 +0900

    feat(packages/design-tokens): 팔레트 세팅

---
+++ packages/design-tokens/src/palette.ts
@@ -0,0 +1,9 @@
+export const palette = {
+  white: "#FFFFFF",
+  gray: "#DCDCDC",
+  darkGray: "#999999",
+  warning: "#FF6161",
+  primary: "#47309C",
+  secondary: "#C3BBE0",
+  tertiary: "#1C133E",
+} as const;
