7793ee31b0ce5d3b585f1b9b3542975f416dfb9c
홍서현 <ghdtjgus76@naver.com>
2025-01-18 20:07:56 +0900

    Feature/#17 typography 세팅 (#83)
    * feat(packages/design-tokens): typography 정의
    
    * feat(packages/design-tokens): 배럴 파일 추가
    
    * chore(packages/design-tokens): 배럴 파일 export 하도록 수정

---
+++ packages/design-tokens/src/typo.ts
@@ -0,0 +1,32 @@
+export const typography = {
+  ["headline-20"]: {
+    fontStyle: "SemiBold",
+    fontSize: 20,
+    lineHeight: 24,
+    letterSpacing: "-2%",
+  },
+  ["body-18"]: {
+    fontStyle: "Medium",
+    fontSize: 18,
+    lineHeight: 20,
+    letterSpacing: "-2%",
+  },
+  ["body-12"]: {
+    fontStyle: "Medium",
+    fontSize: 12,
+    lineHeight: 18,
+    letterSpacing: "-2%",
+  },
+  ["button-14"]: {
+    fontStyle: "SemiBold",
+    fontSize: 14,
+    lineHeight: 24,
+    letterSpacing: "-2%",
+  },
+  ["button-12"]: {
+    fontStyle: "SemiBold",
+    fontSize: 12,
+    lineHeight: 20,
+    letterSpacing: "-2%",
+  },
+};
