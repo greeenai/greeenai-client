395aa6dcac1aba770cbbde1bb5890ab73240fc44
ghdtjgus76 <ghdtjgus76@naver.com>
2025-01-29 13:21:42 +0900

    feat(apps/app): 하단 탭 아이콘 추가 및 적용

--- apps/app/src/navigators/TabNavigator.tsx
+++ apps/app/src/navigators/TabNavigator.tsx
@@ -1,12 +1,11 @@
-import {
-  BottomTabNavigationOptions,
-  createBottomTabNavigator,
-} from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import CreateDiaryScreen from '../screens/Tab/CreateDiary';
 import HomeScreen from '../screens/Tab/Home';
 import SettingScreen from '../screens/Tab/Setting';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {theme} from '@greeenai/design-tokens';
+import Icon from '../components/Icon';
+import {RouteProp} from '@react-navigation/native';
 
 type TabNavigatorParamList = {
   CreateDiary: undefined;
@@ -16,10 +15,20 @@ type TabNavigatorParamList = {
 
 const Tab = createBottomTabNavigator<TabNavigatorParamList>();
 
+const tabBarIconMapping = {
+  Home: <Icon name={'Home'} />,
+  Setting: <Icon name={'Setting'} />,
+  CreateDiary: <Icon name={'Diary'} />,
+};
+
 function TabNavigator() {
   const insets = useSafeAreaInsets();
 
-  const screenOptions: BottomTabNavigationOptions = {
+  const screenOptions = ({
+    route,
+  }: {
+    route: RouteProp<TabNavigatorParamList>;
+  }) => ({
     headerShown: true,
     headerShadowVisible: false,
     headerStyle: {
@@ -28,7 +37,13 @@ function TabNavigator() {
     headerTitleStyle: {
       ...theme.typo['headline-20'],
     },
-  };
+    tabBarStyle: {
+      height: 102,
+      paddingTop: 18,
+    },
+    tabBarLabel: '',
+    tabBarIcon: () => tabBarIconMapping[route.name],
+  });
 
   return (
     <Tab.Navigator initialRouteName={'Home'} screenOptions={screenOptions}>
