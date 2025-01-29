6a700be9f243cfdc56a42f9cc4291101f6bb40bf
ghdtjgus76 <ghdtjgus76@naver.com>
2025-01-29 12:27:17 +0900

    feat(apps/app): 아이콘 컴포넌트 구현

---
+++ apps/app/src/components/Icon/index.tsx
@@ -0,0 +1,34 @@
+import {CSSProperties} from 'react';
+import {KeyOfIcons} from '../../types/Icon';
+import {KeyOfPalette, theme} from '@greeenai/design-tokens';
+import * as Icons from '../../assets/icons';
+
+interface IconProps {
+  name: KeyOfIcons;
+  width?: CSSProperties['width'];
+  height?: CSSProperties['height'];
+  fill?: KeyOfPalette;
+  style?: CSSProperties | CSSProperties[];
+  onPress?: (...args: any[]) => void;
+}
+
+function Icon({
+  name,
+  width = 32,
+  height = 32,
+  fill = 'black',
+  ...props
+}: IconProps) {
+  const SvgIcon = Icons[name];
+
+  return (
+    <SvgIcon
+      width={width}
+      height={height}
+      style={{color: theme.palette[fill]}}
+      {...props}
+    />
+  );
+}
+
+export default Icon;
---
+++ apps/app/src/types/Icon.ts
@@ -0,0 +1,3 @@
+import * as icons from '../assets/icons';
+
+export type KeyOfIcons = keyof typeof icons;
