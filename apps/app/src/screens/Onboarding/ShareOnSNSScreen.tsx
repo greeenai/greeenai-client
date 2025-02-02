8aec489819a1b6252510cedad2d67f7ef9198d79
ghdtjgus76 <ghdtjgus76@naver.com>
2025-02-02 15:51:14 +0900

    chore(apps/app): SNS 공유 온보딩 스크린에 다음 버튼 추가

--- apps/app/src/screens/Onboarding/ShareOnSNSScreen.tsx
+++ apps/app/src/screens/Onboarding/ShareOnSNSScreen.tsx
@@ -1,12 +1,15 @@
-import {Text} from 'react-native';
+import {Text, TouchableOpacity} from 'react-native';
 import ScreenLayout from '../../components/ScreenLayout';
 
-export type ShareOnSNSScreenProps = {};
+export type ShareOnSNSScreenProps = {onNext: () => void};
 
-function ShareOnSNSScreen({}: ShareOnSNSScreenProps) {
+function ShareOnSNSScreen({onNext}: ShareOnSNSScreenProps) {
   return (
     <ScreenLayout isSafeArea>
       <Text>ShareOnSNS Screen</Text>
+      <TouchableOpacity onPress={onNext}>
+        <Text>다음</Text>
+      </TouchableOpacity>
     </ScreenLayout>
   );
 }
