ea85a291d5d3bba00cd4be5f10993116d6280bd6
ghdtjgus76 <ghdtjgus76@naver.com>
2025-02-09 17:43:53 +0900

    feat(apps/app): Typography 컴포넌트 구현

---
+++ apps/app/src/components/Typography.tsx
@@ -0,0 +1,27 @@
+import {KeyOfPalette, KeyOfTypo, theme} from '@greeenai/design-tokens';
+import {ReactNode} from 'react';
+import {Text, ViewStyle} from 'react-native';
+
+type TypographyProps = {
+  children: ReactNode;
+  type?: KeyOfTypo;
+  color?: KeyOfPalette;
+  style?: ViewStyle;
+};
+
+function Typography({
+  children,
+  type = 'body-12',
+  color = 'black',
+  style,
+}: TypographyProps) {
+  const typographyStyle = theme.typo[type];
+
+  return (
+    <Text style={[typographyStyle, {color: theme.palette[color]}, style]}>
+      {children}
+    </Text>
+  );
+}
+
+export default Typography;
