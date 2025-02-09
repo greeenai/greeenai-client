04540cb5176ab7ef3f9732a4e61da8f78a61eb08
ghdtjgus76 <ghdtjgus76@naver.com>
2025-02-09 20:29:17 +0900

    docs(apps/app): SocialLoginButton 컴포넌트 스토리북 작성

---
+++ apps/app/src/components/SocialLoginButton/SocialLoginButton.stories.tsx
@@ -0,0 +1,15 @@
+import SocialLoginButton from '.';
+
+export default {
+  title: 'UI/Button',
+  component: SocialLoginButton,
+  tags: ['autodocs'],
+};
+
+export const Kakao = {
+  args: {variant: 'kakao'},
+};
+
+export const Apple = {
+  args: {variant: 'apple'},
+};
