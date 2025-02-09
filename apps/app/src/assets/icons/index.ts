059a37cbcc53da0bba4f72286dfc602d5f753ad6
ghdtjgus76 <ghdtjgus76@naver.com>
2025-02-09 18:41:10 +0900

    feat(apps/app): 버튼에서 사용할 svg 이미지 export 구문 추가

--- apps/app/src/assets/icons/index.ts
+++ apps/app/src/assets/icons/index.ts
@@ -2,3 +2,5 @@ export {default as LeftChevron} from './left-chevron.svg';
 export {default as Diary} from './diary.svg';
 export {default as Home} from './home.svg';
 export {default as Setting} from './setting.svg';
+export {default as Share} from './share.svg';
+export {default as Download} from './download.svg';
