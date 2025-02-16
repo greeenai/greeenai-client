eaa1cc8a570876a17d5b6817a6da40adcb278f27
ghdtjgus76 <ghdtjgus76@naver.com>
2025-02-16 12:33:09 +0900

    docs(apps/app): Checkbox 컴포넌트 스토리북 작성

---
+++ apps/app/src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,35 @@
+import {useState} from 'react';
+import Checkbox from '.';
+
+export default {
+  title: 'UI/Checkbox',
+  component: Checkbox,
+  tags: ['autodocs'],
+};
+
+export const Checked = {
+  args: {
+    defaultChecked: true,
+  },
+};
+
+export const UnChecked = {
+  args: {
+    defaultChecked: false,
+  },
+};
+
+export const Controlled = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChangeIsChecked = () => {
+    setIsChecked(prevIsChecked => !prevIsChecked);
+  };
+  return <Checkbox isChecked={isChecked} onChange={handleChangeIsChecked} />;
+};
+
+export const Disabled = {
+  args: {
+    isDisabled: true,
+  },
+};
