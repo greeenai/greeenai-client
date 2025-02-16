2e81a4982c173ddf2baeeab72331b0bf8deb9334
ghdtjgus76 <ghdtjgus76@naver.com>
2025-02-16 14:51:07 +0900

    feat(apps/app): Divider 컴포넌트 구현

---
+++ apps/app/src/components/Divider/index.tsx
@@ -0,0 +1,31 @@
+import {KeyOfPalette} from '@greeenai/design-tokens';
+import {StyleProp, View, ViewStyle} from 'react-native';
+
+type DividerProps = {
+  color?: KeyOfPalette;
+  thickness?: number;
+  isHorizontal?: boolean;
+  style?: StyleProp<ViewStyle>;
+};
+
+function Divider({
+  color = 'gray',
+  thickness = 1,
+  isHorizontal = true,
+  style,
+}: DividerProps) {
+  return (
+    <View
+      style={[
+        {
+          backgroundColor: color,
+          width: isHorizontal ? '100%' : thickness,
+          height: isHorizontal ? thickness : '100%',
+        },
+        style,
+      ]}
+    />
+  );
+}
+
+export default Divider;
