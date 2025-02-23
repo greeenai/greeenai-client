c14c0f459b410e6a1b6314188f050e9bcf43729a
ghdtjgus76 <ghdtjgus76@naver.com>
2025-02-23 16:35:05 +0900

    feat(apps/app): useAuthStorage 훅 추가

---
+++ apps/app/src/hooks/useAuthStorage.ts
@@ -0,0 +1,42 @@
+import AsyncStorageService from '../utils/AsyncStorageService';
+
+function useAuthStorage() {
+  const setAuthData = async (accessToken: string, refreshToken: string) => {
+    await AsyncStorageService.multiSet([
+      {key: AsyncStorageKey.accessToken, value: accessToken},
+      {key: AsyncStorageKey.refreshToken, value: refreshToken},
+    ]);
+  };
+
+  const getAuthData = async () => {
+    return await AsyncStorageService.multiGet([
+      AsyncStorageKey.accessToken,
+      AsyncStorageKey.refreshToken,
+    ]);
+  };
+
+  const getAccessToken = async () => {
+    return await AsyncStorageService.getItem(AsyncStorageKey.accessToken);
+  };
+
+  const getRefreshToken = async () => {
+    return await AsyncStorageService.getItem(AsyncStorageKey.refreshToken);
+  };
+
+  const clearAuthData = async () => {
+    await AsyncStorageService.multiSet([
+      {key: AsyncStorageKey.accessToken, value: null},
+      {key: AsyncStorageKey.refreshToken, value: null},
+    ]);
+  };
+
+  return {
+    setAuthData,
+    getAuthData,
+    getAccessToken,
+    getRefreshToken,
+    clearAuthData,
+  };
+}
+
+export default useAuthStorage;
