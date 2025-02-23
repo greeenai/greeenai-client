d995b58827ea3cba06f82dfb6da643413081959a
ghdtjgus76 <ghdtjgus76@naver.com>
2025-02-23 10:57:43 +0900

    design(apps/app): 온보딩 나머지 페이지 퍼블리싱

--- apps/app/src/screens/Onboarding/ChooseAnswerScreen.tsx
+++ apps/app/src/screens/Onboarding/ChooseAnswerScreen.tsx
@@ -1,5 +1,6 @@
-import {Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import ScreenLayout from '../../components/@common/ScreenLayout';
+import Button from '../../components/@common/Button';
 
 export type ChooseAnswerScreenProps = {
   onNext: () => void;
@@ -8,12 +9,23 @@ export type ChooseAnswerScreenProps = {
 function ChooseAnswerScreen({onNext}: ChooseAnswerScreenProps) {
   return (
     <ScreenLayout isSafeArea>
-      <Text>ChooseAnswer Screen</Text>
-      <TouchableOpacity onPress={onNext}>
-        <Text>다음</Text>
-      </TouchableOpacity>
+      <View style={chooseAnswerScreenStyle.content} />
+      <View style={chooseAnswerScreenStyle.bottomButtonContainer}>
+        <Button onPress={onNext}>다음</Button>
+      </View>
     </ScreenLayout>
   );
 }
 
 export default ChooseAnswerScreen;
+
+const chooseAnswerScreenStyle = StyleSheet.create({
+  content: {
+    flex: 1,
+  },
+  bottomButtonContainer: {
+    marginBottom: 20,
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
--- apps/app/src/screens/Onboarding/ShareOnSNSScreen.tsx
+++ apps/app/src/screens/Onboarding/ShareOnSNSScreen.tsx
@@ -1,17 +1,29 @@
-import {Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import ScreenLayout from '../../components/@common/ScreenLayout';
+import Button from '../../components/@common/Button';
 
 export type ShareOnSNSScreenProps = {onNext: () => void};
 
 function ShareOnSNSScreen({onNext}: ShareOnSNSScreenProps) {
   return (
     <ScreenLayout isSafeArea>
-      <Text>ShareOnSNS Screen</Text>
-      <TouchableOpacity onPress={onNext}>
-        <Text>다음</Text>
-      </TouchableOpacity>
+      <View style={shareOnSNSScreenStyle.content} />
+      <View style={shareOnSNSScreenStyle.bottomButtonContainer}>
+        <Button onPress={onNext}>다음</Button>
+      </View>
     </ScreenLayout>
   );
 }
 
 export default ShareOnSNSScreen;
+
+const shareOnSNSScreenStyle = StyleSheet.create({
+  content: {
+    flex: 1,
+  },
+  bottomButtonContainer: {
+    marginBottom: 20,
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
