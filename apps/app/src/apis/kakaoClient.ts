2f95122128d7ca8086c04dd6f255787017337a13
ghdtjgus76 <ghdtjgus76@naver.com>
2025-03-02 12:56:25 +0900

    chore(apps/app): kakaoClient 각 함수 반환 값 타입 명확하게 지정

--- apps/app/src/apis/kakaoClient.ts
+++ apps/app/src/apis/kakaoClient.ts
@@ -1,13 +1,15 @@
 import {
   getProfile,
+  KakaoOAuthToken,
+  KakaoProfile,
   loginWithKakaoAccount,
 } from '@react-native-seoul/kakao-login';
 
 const kakaoClient = {
-  loginWithKakaoAccount: async () => {
+  loginWithKakaoAccount: async (): Promise<KakaoOAuthToken> => {
     return await loginWithKakaoAccount();
   },
-  getProfile: async () => {
+  getProfile: async (): Promise<KakaoProfile> => {
     return await getProfile();
   },
 };
