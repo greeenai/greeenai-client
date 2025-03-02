a6897ec17102a3a67219472244ed8d99a08bd19e
홍서현 <ghdtjgus76@naver.com>
2025-03-02 15:22:03 +0900

    Feature/#138 설정 탭 퍼블리싱 (#139)
    * design(apps/app): 온보딩 페이지 ScreenLayout isSafeArea 속성 삭제
    
    * fix(apps/app): Divider 팔레트 색상 적용 안 되는 문제 해결
    
    * feat(apps/app): 설정 페이지 svg 이미지 추가
    
    * feat(apps/app): 설정 탭 퍼블리싱
    
    * design(apps/app): menuItem gap 값 수정

--- apps/app/src/screens/Tab/Setting.tsx
+++ apps/app/src/screens/Tab/Setting.tsx
@@ -1,12 +1,59 @@
-import {Text} from 'react-native';
 import ScreenLayout from '../../components/@common/ScreenLayout';
+import Typography from '../../components/@common/Typography';
+import Divider from '../../components/@common/Divider';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import Icon from '../../components/@common/Icon';
 
 function SettingScreen() {
   return (
-    <ScreenLayout>
-      <Text>Setting Screen</Text>
+    <ScreenLayout style={settingScreenStyle.container}>
+      <Typography color={'darkGray'} style={settingScreenStyle.categoryText}>
+        일반
+      </Typography>
+      <Divider />
+      <TouchableOpacity style={settingScreenStyle.menuItem}>
+        <Icon name={'TermsAgreement'} />
+        <Typography type={'body-18'}>약관</Typography>
+      </TouchableOpacity>
+      <Divider style={settingScreenStyle.divider} />
+      <Typography color={'darkGray'} style={settingScreenStyle.categoryText}>
+        회원 관리
+      </Typography>
+      <Divider />
+      <TouchableOpacity style={settingScreenStyle.menuItem}>
+        <Icon name={'LogOut'} />
+        <Typography type={'body-18'}>로그아웃</Typography>
+      </TouchableOpacity>
+      <Divider />
+      <TouchableOpacity style={settingScreenStyle.menuItem}>
+        <Icon name={'DeleteAccount'} />
+        <Typography type={'body-18'}>회원 탈퇴</Typography>
+      </TouchableOpacity>
+      <Divider />
     </ScreenLayout>
   );
 }
 
 export default SettingScreen;
+
+const settingScreenStyle = StyleSheet.create({
+  container: {
+    alignItems: 'flex-start',
+  },
+  categoryText: {
+    marginLeft: 30,
+    marginBottom: 10,
+  },
+  menuItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    gap: 12,
+    width: '100%',
+  },
+  divider: {
+    marginBottom: 40,
+  },
+});
