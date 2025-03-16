17ef1c5f3bab4dcabac12ddf5b603f7ad38846ca
ghdtjgus76 <ghdtjgus76@naver.com>
2025-03-16 13:10:30 +0900

    feat: 객관식 답변 선택하기 온보딩 페이지 퍼블리싱

--- apps/app/src/screens/Onboarding/ChooseAnswerScreen.tsx
+++ apps/app/src/screens/Onboarding/ChooseAnswerScreen.tsx
@@ -1,6 +1,7 @@
 import {StyleSheet, View} from 'react-native';
 import ScreenLayout from '../../components/@common/ScreenLayout';
 import Button from '../../components/@common/Button';
+import Typography from '../../components/@common/Typography';
 
 export type ChooseAnswerScreenProps = {
   onNext: () => void;
@@ -8,8 +9,17 @@ export type ChooseAnswerScreenProps = {
 
 function ChooseAnswerScreen({onNext}: ChooseAnswerScreenProps) {
   return (
-    <ScreenLayout>
-      <View style={chooseAnswerScreenStyle.content} />
+    <ScreenLayout isSafeArea>
+      <View style={chooseAnswerScreenStyle.content}>
+        <Typography
+          type={'headline-20'}
+          style={chooseAnswerScreenStyle.mainText}>
+          {'당신의 하루를\n자세히 들려주세요'}
+        </Typography>
+        <Typography>
+          {'사진만으로 읽기 어려운 이야기를 \n객관식으로 답해보아요'}
+        </Typography>
+      </View>
       <View style={chooseAnswerScreenStyle.bottomButtonContainer}>
         <Button onPress={onNext}>다음</Button>
       </View>
@@ -22,6 +32,12 @@ export default ChooseAnswerScreen;
 const chooseAnswerScreenStyle = StyleSheet.create({
   content: {
     flex: 1,
+    width: '100%',
+    paddingVertical: 80,
+    paddingHorizontal: 40,
+  },
+  mainText: {
+    marginBottom: 15,
   },
   bottomButtonContainer: {
     marginBottom: 20,
