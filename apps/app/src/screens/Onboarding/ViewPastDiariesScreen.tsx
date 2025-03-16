441aef299401b7e1733a10acff34886aad67c609
홍서현 <ghdtjgus76@naver.com>
2025-03-16 13:11:45 +0900

    Design/#156 온보딩 페이지 퍼블리싱 (#159)
    * feat: 그림 일기 생성하기 온보딩 페이지 퍼블리싱
    
    * feat: 객관식 답변 선택하기 온보딩 페이지 퍼블리싱
    
    * design: 불필요한 borderWidth 속성 삭제
    
    * feat: SNS 공유하기 온보딩 페이지 퍼블리싱
    
    * feat: 지난 일기 다시 보기 온보딩 페이지 퍼블리싱

--- apps/app/src/screens/Onboarding/ViewPastDiariesScreen.tsx
+++ apps/app/src/screens/Onboarding/ViewPastDiariesScreen.tsx
@@ -1,6 +1,7 @@
 import {StyleSheet, View} from 'react-native';
 import ScreenLayout from '../../components/@common/ScreenLayout';
 import Button from '../../components/@common/Button';
+import Typography from '../../components/@common/Typography';
 
 export type ViewPastDiariesScreenProps = {
   onNext: () => void;
@@ -8,8 +9,17 @@ export type ViewPastDiariesScreenProps = {
 
 function ViewPastDiariesScreen({onNext}: ViewPastDiariesScreenProps) {
   return (
-    <ScreenLayout>
-      <View style={viewPastDiariesScreenStyle.content} />
+    <ScreenLayout isSafeArea>
+      <View style={viewPastDiariesScreenStyle.content}>
+        <Typography
+          type={'headline-20'}
+          style={viewPastDiariesScreenStyle.mainText}>
+          {'지난 일기도\n다시 볼 수 있어요'}
+        </Typography>
+        <Typography>
+          {'사진만으로 읽기 어려운 이야기를 \n객관식으로 답해보아요'}
+        </Typography>
+      </View>
       <View style={viewPastDiariesScreenStyle.bottomButtonContainer}>
         <Button onPress={onNext}>다음</Button>
       </View>
@@ -22,6 +32,12 @@ export default ViewPastDiariesScreen;
 const viewPastDiariesScreenStyle = StyleSheet.create({
   content: {
     flex: 1,
+    width: '100%',
+    paddingVertical: 80,
+    paddingHorizontal: 40,
+  },
+  mainText: {
+    marginBottom: 15,
   },
   bottomButtonContainer: {
     marginBottom: 20,
