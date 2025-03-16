29a4a032261442dcced38046a8373da716a49b44
ghdtjgus76 <ghdtjgus76@naver.com>
2025-03-16 13:10:53 +0900

    feat: SNS 공유하기 온보딩 페이지 퍼블리싱

--- apps/app/src/screens/Onboarding/ShareOnSNSScreen.tsx
+++ apps/app/src/screens/Onboarding/ShareOnSNSScreen.tsx
@@ -1,13 +1,21 @@
 import {StyleSheet, View} from 'react-native';
 import ScreenLayout from '../../components/@common/ScreenLayout';
 import Button from '../../components/@common/Button';
+import Typography from '../../components/@common/Typography';
 
 export type ShareOnSNSScreenProps = {onNext: () => void};
 
 function ShareOnSNSScreen({onNext}: ShareOnSNSScreenProps) {
   return (
-    <ScreenLayout>
-      <View style={shareOnSNSScreenStyle.content} />
+    <ScreenLayout isSafeArea>
+      <View style={shareOnSNSScreenStyle.content}>
+        <Typography type={'headline-20'} style={shareOnSNSScreenStyle.mainText}>
+          {'지난 일기도\n다시 볼 수 있어요'}
+        </Typography>
+        <Typography>
+          {'사진만으로 읽기 어려운 이야기를 \n객관식으로 답해보아요'}
+        </Typography>
+      </View>
       <View style={shareOnSNSScreenStyle.bottomButtonContainer}>
         <Button onPress={onNext}>다음</Button>
       </View>
@@ -20,6 +28,12 @@ export default ShareOnSNSScreen;
 const shareOnSNSScreenStyle = StyleSheet.create({
   content: {
     flex: 1,
+    width: '100%',
+    paddingVertical: 80,
+    paddingHorizontal: 40,
+  },
+  mainText: {
+    marginBottom: 15,
   },
   bottomButtonContainer: {
     marginBottom: 20,
