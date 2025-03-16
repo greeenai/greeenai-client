fe24d047c2896d9db049082c772a087c63eb23eb
ghdtjgus76 <ghdtjgus76@naver.com>
2025-03-16 13:06:44 +0900

    feat: 그림 일기 생성하기 온보딩 페이지 퍼블리싱

--- apps/app/src/screens/Onboarding/SelectPhotoScreen.tsx
+++ apps/app/src/screens/Onboarding/SelectPhotoScreen.tsx
@@ -1,6 +1,7 @@
 import {StyleSheet, View} from 'react-native';
 import ScreenLayout from '../../components/@common/ScreenLayout';
 import Button from '../../components/@common/Button';
+import Typography from '../../components/@common/Typography';
 
 export type SelectPhotoScreenProps = {
   onNext: () => void;
@@ -8,8 +9,17 @@ export type SelectPhotoScreenProps = {
 
 function SelectPhotoScreen({onNext}: SelectPhotoScreenProps) {
   return (
-    <ScreenLayout>
-      <View style={selectPhotoScreenStyle.content} />
+    <ScreenLayout isSafeArea>
+      <View style={selectPhotoScreenStyle.content}>
+        <Typography
+          type={'headline-20'}
+          style={selectPhotoScreenStyle.mainText}>
+          {'사진 3장이면 \n충분해요'}
+        </Typography>
+        <Typography>
+          {'사진만으로 읽기 어려운 이야기를 \n객관식으로 답해보아요'}
+        </Typography>
+      </View>
       <View style={selectPhotoScreenStyle.bottomButtonContainer}>
         <Button onPress={onNext}>다음</Button>
       </View>
@@ -22,6 +32,13 @@ export default SelectPhotoScreen;
 const selectPhotoScreenStyle = StyleSheet.create({
   content: {
     flex: 1,
+    width: '100%',
+    borderWidth: 1,
+    paddingVertical: 80,
+    paddingHorizontal: 40,
+  },
+  mainText: {
+    marginBottom: 15,
   },
   bottomButtonContainer: {
     marginBottom: 20,
