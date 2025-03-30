f272645ae51a023a641cebd355c40a74c873a140
홍서현 <ghdtjgus76@naver.com>
2025-03-30 18:41:34 +0900

    Feature/#169 그림 일기 갤러리 저장 기능 구현 (#172)
    * feat(apps/app): @react-native-camera-roll/camera-roll 라이브러리 설치
    
    * feat(apps/app): rn-fetch-blob 라이브러리 설치
    
    * feat(apps/app): pod file 변경사항 반영
    
    * feat(apps/app): 이미지 갤러리 저장 기능 구현
    
    * remove(apps/app): rn-fetch-blob 라이브러리 삭제
    
    * remove(apps/app): 이미지 로컬에 저장하는 로직 삭제
    
    * feat(apps/app): react-native-view-shot 라이브러리 설치
    
    * feat(apps/app): pod file 변경사항 반영
    
    * feat(apps/app): 그림 일기 저장 기능 구현
    
    * design(apps/app): 지난 일기 flat list 컨테이너 스타일 변경

--- apps/app/src/screens/BottomTab/PastDiary/DiaryContentScreen.tsx
+++ apps/app/src/screens/BottomTab/PastDiary/DiaryContentScreen.tsx
@@ -1,4 +1,4 @@
-import {useLayoutEffect} from 'react';
+import {useLayoutEffect, useRef} from 'react';
 import {Alert, Image, ScrollView, StyleSheet, View} from 'react-native';
 import {SCREEN_WIDTH} from '@gorhom/bottom-sheet';
 import {useNavigation, useRoute, RouteProp} from '@react-navigation/native';
@@ -10,12 +10,14 @@ import {mockDiaryContent} from '../../../constants/mockDatas/diaryContent';
 import Typography from '../../../components/@common/Typography';
 import Button from '../../../components/@common/Button';
 import Icon from '../../../components/@common/Icon';
-import {downloadImageToLocal} from '../../../utils/downloadImageInLocal';
+import {captureRef} from 'react-native-view-shot';
 
 function DiaryContentScreen() {
   const navigation = useNavigation();
   const route =
     useRoute<RouteProp<PastDiaryStackNavigatorParamList, 'DiaryContent'>>();
+  const scrollViewRef = useRef(null);
+  const contentRef = useRef(null);
 
   const createdAt = route.params.createdAt;
   const formattedContent = mockDiaryContent.content.replace(
@@ -25,11 +27,17 @@ function DiaryContentScreen() {
 
   const handlePressSaveDiaryImage = async () => {
     try {
-      const imageUrl = mockDiaryContent.imageUrl;
-      const localImagePath = await downloadImageToLocal(imageUrl);
+      if (scrollViewRef.current) {
+        const uri = await captureRef(scrollViewRef, {
+          format: 'jpg',
+          quality: 0.9,
+          result: 'tmpfile',
+          snapshotContentContainer: true,
+        });
 
-      await CameraRoll.saveAsset(localImagePath, {type: 'photo'});
-      Alert.alert('저장 완료', '이미지가 갤러리에 저장되었습니다.');
+        await CameraRoll.saveAsset(uri, {type: 'photo'});
+        Alert.alert('저장 완료', '이미지가 갤러리에 저장되었습니다.');
+      }
     } catch (error) {
       console.error('이미지 저장 실패:', error);
       Alert.alert('저장 실패', '이미지를 갤러리에 저장하는데 실패했습니다.');
@@ -44,33 +52,37 @@ function DiaryContentScreen() {
 
   return (
     <ScreenLayout>
-      <ScrollView>
-        <View style={diaryContentScreenStyle.imageContainer}>
-          <Image
-            source={{uri: mockDiaryContent.imageUrl}}
-            style={diaryContentScreenStyle.image}
-          />
-        </View>
-        <View style={diaryContentScreenStyle.contentContainer}>
-          <Typography type={'headline-20'}>{mockDiaryContent.title}</Typography>
-          <Typography type={'body-12'}>{formattedContent}</Typography>
-        </View>
-        <View style={diaryContentScreenStyle.buttonContainer}>
-          <Button
-            leftElement={<Icon name={'SaveInGallery'} width={20} height={20} />}
-            size={'sm'}
-            onPress={handlePressSaveDiaryImage}>
-            저장하기
-          </Button>
-          <Button
-            leftElement={<Icon name={'ShareOnSNS'} width={20} height={20} />}
-            size={'sm'}
-            backgroundColor="white"
-            onPress={() => {}}>
-            공유하기
-          </Button>
+      <ScrollView ref={scrollViewRef} collapsable={false}>
+        <View collapsable={false} ref={contentRef}>
+          <View style={diaryContentScreenStyle.imageContainer}>
+            <Image
+              source={{uri: mockDiaryContent.imageUrl}}
+              style={diaryContentScreenStyle.image}
+            />
+          </View>
+          <View style={diaryContentScreenStyle.contentContainer}>
+            <Typography type={'headline-20'}>
+              {mockDiaryContent.title}
+            </Typography>
+            <Typography type={'body-12'}>{formattedContent}</Typography>
+          </View>
         </View>
       </ScrollView>
+      <View style={diaryContentScreenStyle.buttonContainer}>
+        <Button
+          leftElement={<Icon name={'SaveInGallery'} width={20} height={20} />}
+          size={'sm'}
+          onPress={handlePressSaveDiaryImage}>
+          저장하기
+        </Button>
+        <Button
+          leftElement={<Icon name={'ShareOnSNS'} width={20} height={20} />}
+          size={'sm'}
+          backgroundColor="white"
+          onPress={() => {}}>
+          공유하기
+        </Button>
+      </View>
     </ScreenLayout>
   );
 }
@@ -98,6 +110,6 @@ const diaryContentScreenStyle = StyleSheet.create({
     justifyContent: 'center',
     gap: 15,
     paddingHorizontal: 25,
-    paddingBottom: 15,
+    paddingTop: 15,
   },
 });
