69f601062011970dd3ac1a36712c8a9fc0999572
ghdtjgus76 <ghdtjgus76@naver.com>
2025-05-18 14:01:13 +0900

    comment(apps/app): 일기 api 주석 추가

--- apps/app/src/apis/diary/index.ts
+++ apps/app/src/apis/diary/index.ts
@@ -7,11 +7,13 @@ import {
 } from './index.types';
 
 class DiaryApi {
+  // 지난 일기 리스트 조회
   static async getLastDiaries() {
     const response = await fetcherInstance.get('/diaries');
     return response as ApiResponse<LastDiariesResponse>;
   }
 
+  // 일기 질문 리스트 조회
   static async getDiaryQuestions(requestBody: DiaryQuestionRequestBody) {
     const response = await fetcherInstance.post('/diaries', requestBody);
     return response as ApiResponse<DiaryQuestionResponseDto>;
